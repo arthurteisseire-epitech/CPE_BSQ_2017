b61e6e6ea47db2aec58182603afd5c27aa9ec855
Arthur <arthur.teisseire@epitech.eu>
2017-12-11 19:48:27 +0100

    local_bsq done : not tested yet; Need to read file to process

---
+++ src/local_bsq.c
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2017
+** File Name : local_bsq.c
+** File description:
+** By Arthur Teisseire
+*/
+
+#include "bsq.h"
+
+void local_bsq(bsq_t *bsq, int row, int col)
+{
+	int size;
+	int size_y = bsq->nb_col;
+	int x = row;
+	int y = col;
+
+	while (x < bsq->nb_row && bsq->map[x][y] != 'o') {
+		while (bsq->map[x][y] == '.')
+			y++;
+		if (y - col < size)
+			size_y = y - col;
+		x++;
+		y = col;
+	}
+	size = (size_y <= x ? size_y : x);
+	if (size > bsq->size) {
+		bsq->size = size;
+		set_pos(&bsq->pos, row, col);
+	}
+}
+
+void set_pos(pos_t *pos, int row, int col)
+{
+	pos->x = col;
+	pos->y = row;
+}
