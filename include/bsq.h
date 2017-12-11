60ad1b2fa1f5b600c3a6c0423fd992cf1570c24b
Arthur <arthur.teisseire@epitech.eu>
2017-12-11 19:39:11 +0100

    struct bsq and pos added

---
+++ include/bsq.h
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2017
+** File Name : bsq.h
+** File description:
+** By Arthur Teisseire
+*/
+
+#ifndef BSQ_H
+#define BSQ_H
+
+typedef struct pos {
+	int x;
+	int y;
+} pos_t;
+
+typedef struct bsq {
+	char **map;
+	int nb_col;
+	int nb_row;
+	int size;
+	pos_t pos;
+} bsq_t;
+
+void local_bsq(bsq_t *bsq, int row, int col);
+
+#endif
