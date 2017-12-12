cdecc5d956f59521c2839959afeab4b89f7a310a
Arthur <arthur.teisseire@epitech.eu>
2017-12-12 23:43:49 +0100

    Now it works for all : optimized code only missing

--- src/local_bsq.c
+++ src/local_bsq.c
@@ -11,6 +11,7 @@ void local_bsq(bsq_t *bsq, int row, int col)
 {
 	int size;
 	int size_y = bsq->nb_col;
+	int size_x = bsq->nb_row;
 	int x = row;
 	int y = col;
 
@@ -21,14 +22,21 @@ void local_bsq(bsq_t *bsq, int row, int col)
 			return;
 		if (y - col < size_y)
 			size_y = y - col;
+		size_x = x - (row - 1);
+		if (size_x > bsq->size && size_y > bsq->size) {
+			bsq->size = (size_y < size_x ? size_y : size_x);
+			set_pos(&bsq->pos, row, col);
+		}
 		x++;
 		y = col;
 	}
+	/*
 	size = (size_y < x - row ? size_y : x - row);
 	if (size > bsq->size) {
 		bsq->size = size;
 		set_pos(&bsq->pos, row, col);
 	}
+	*/
 }
 
 void set_pos(pos_t *pos, int row, int col)
